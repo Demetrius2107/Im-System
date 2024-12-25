216a2f51293e3c32e5c61b8f7266e7a2d56c6e79
ShuKun Li <Shukun.Li@connext.com.cn>
2024-12-25 13:55:24 +0800

    feat: 好友组Enum补充

--- im-common/src/main/java/com/hua/im/imcommon/exception/ApplicationExceptionEnum.java
+++ im-common/src/main/java/com/hua/im/imcommon/exception/ApplicationExceptionEnum.java
@@ -1,8 +1,5 @@
 package com.hua.im.imcommon.exception;
 
-/**
- * @author Shukun.Li
- */
 public interface ApplicationExceptionEnum {
 
     int getCode();
---
+++ im-common/src/main/java/com/hua/im/imcommon/model/UserClientDto.java
@@ -0,0 +1,19 @@
+package com.hua.im.imcommon.model;
+
+import lombok.Data;
+
+/**
+ * @author Shukun.Li
+ */
+@Data
+public class UserClientDto {
+
+    private Integer appId;
+
+    private Integer clientType;
+
+    private String userId;
+
+    private String imei;
+
+}
