dd485fd44ea9fe477d0c920f05b592f584a16e40
Lipschultz <2562017477@qq.com>
2025-03-03 23:34:13 +0800

    feat: mq传输存储群组消息

--- im-message-store/src/main/java/com/lip/message/mq/StoreP2PMessageReceiver.java
+++ im-message-store/src/main/java/com/lip/message/mq/StoreP2PMessageReceiver.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.lip.constants.Constants;
 import com.lip.message.dao.ImMessageBodyEntity;
-import com.lip.message.model.DoStoreGroupMessageDto;
 import com.lip.message.model.DoStoreP2PMessageDto;
 import com.lip.message.service.StoreMessageService;
 import com.rabbitmq.client.Channel;
@@ -18,54 +17,56 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * @author: Elon
- * @title: StoreGroupMessageReceiver
+ * @title: StroeP2PMessageReceiver
  * @projectName: IM-System
  * @description:
  * @date: 2025/3/3 19:48
  */
 @Service
-public class StoreGroupMessageReceiver {
+public class StoreP2PMessageReceiver {
 
-
-    private static Logger logger = LoggerFactory.getLogger(StoreGroupMessageReceiver.class);
-
+    private static Logger logger = LoggerFactory.getLogger(StoreP2PMessageReceiver.class);
 
     @Autowired
     StoreMessageService storeMessageService;
 
 
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = Constants.RabbitConstants.StoreP2PMessage, durable = "true"),
+                    exchange = @Exchange(value = Constants.RabbitConstants.StoreP2PMessage, durable = "true")
+            ), concurrency = "1"
+    )
     public void onChatMessage(@Payload Message message,
-                              @Headers Map<String,Object> headers,
+                              @Header Map<String, Object> headers,
                               Channel channel) throws IOException {
-        String msg = new String(message.getBody(),"utf-8");
-        logger.info("CHAT MSG FROM QUEUE ::: {}",msg);
-        Long deliverTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        String msg = new String(message.getBody(), "utf-8");
+        logger.info("CHAT MSG FROM QUEUE ::: {}", msg);
+        Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
 
-        try{
+        try {
             JSONObject jsonObject = JSON.parseObject(msg);
-            DoStoreGroupMessageDto doStoreGroupMessageDto = jsonObject.toJavaObject(DoStoreGroupMessageDto.class);
+            DoStoreP2PMessageDto doStoreP2PMessageDto = jsonObject.toJavaObject(DoStoreP2PMessageDto.class);
             ImMessageBodyEntity messageBody = jsonObject.getObject("messageBody", ImMessageBodyEntity.class);
-            doStoreGroupMessageDto.setImMessageBodyEntity(messageBody);
-            storeMessageService.doStoreGroupMessage(doStoreGroupMessageDto);
-            channel.basicAck(deliverTag, false);
-        } catch (IOException e) {
-            logger.error("处理消息出现异常：{}", e.getMessage());
+            doStoreP2PMessageDto.setImMessageBodyEntity(messageBody);
+            storeMessageService.doStoreP2PMessage(doStoreP2PMessageDto);
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            logger.error("处理消息时出现异常:{}", e.getMessage());
             logger.error("RMQ_CHAT_TRAN_ERROR", e);
             logger.error("NACK_MSG:{}", msg);
-            //第一个false 表示不批量拒绝，第二个false表示不重回队列
-            channel.basicNack(deliverTag, false, false);
+            // 第一个false 标识不批量拒绝 第二个false标识不重回队列
+            channel.basicNack(deliveryTag, false, false);
         }
+
     }
 
 }
