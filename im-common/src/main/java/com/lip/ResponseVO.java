25ecd5d4ae850415df45027547b1c0f08578e85c
Lipschultz <2562017477@qq.com>
2025-03-03 18:55:58 +0800

    feat constant enums

---
+++ im-common/src/main/java/com/lip/ResponseVO.java
@@ -0,0 +1,11 @@
+package com.lip;
+
+/**
+ * @author: Elon
+ * @title: ResponseVO
+ * @projectName: IM-System
+ * @description: TODO
+ * @date: 2025/3/3 18:44
+ */
+public class ResponseVO {
+}
---
+++ im-common/src/main/java/com/lip/exception/ApplicationException.java
@@ -0,0 +1,47 @@
+package com.lip.exception;
+
+/**
+ * @author: Elon
+ * @title: ApplicationException
+ * @projectName: IM-System
+ * @description:
+ * @date: 2025/3/3 18:45
+ */
+public class ApplicationException extends RuntimeException {
+
+    private int code;
+
+    private String error;
+
+
+    public ApplicationException(int code, String message) {
+        super(message);
+        this.code = code;
+        this.error = message;
+    }
+
+    public ApplicationException(ApplicationExceptionEnum exceptionEnum) {
+        super(exceptionEnum.getError());
+        this.code   = exceptionEnum.getCode();
+        this.error  = exceptionEnum.getError();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+
+    /**
+     *  avoid the expensive and useless stack trace for api exceptions
+     *  @see Throwable#fillInStackTrace()
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+
+}
