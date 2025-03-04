ffa63972abda75508ea615ec93a95f3708ce8eb3
Lipschultz <2562017477@qq.com>
2025-03-05 00:22:56 +0800

    feat: Feign远程调用接口-检查发送消息

---
+++ im-tcp/src/main/java/com/lip/fegin/FeignMessageService.java
@@ -0,0 +1,20 @@
+package com.lip.fegin;
+
+import com.lip.ResponseVO;
+import com.lip.model.message.CheckSendMessageReq;
+import feign.Headers;
+import feign.RequestLine;
+
+/**
+ * @author: Elon
+ * @title: FeignMessageService
+ * @projectName: IM-System
+ * @description:
+ * @date: 2025/3/5 0:17
+ */
+public interface FeignMessageService {
+
+    @Headers({"Content-Type: application/json", "Accept:application/json"})
+    @RequestLine("POST /message/checkSend")
+    public ResponseVO checkSendMessage(CheckSendMessageReq o);
+}
