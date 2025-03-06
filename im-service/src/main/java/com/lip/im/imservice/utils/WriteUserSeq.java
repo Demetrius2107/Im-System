e6f2dd5a407e6e6b6be06d944757e1aea7f4aa02
Lipschultz <2562017477@qq.com>
2025-03-06 19:53:12 +0800

    feat: 消息生产者

---
+++ im-service/src/main/java/com/lip/im/imservice/utils/WriteUserSeq.java
@@ -0,0 +1,29 @@
+package com.lip.im.imservice.utils;
+
+
+import com.lip.im.model.constants.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:
+ * @author: lld
+ * @version: 1.0
+ */
+@Service
+public class WriteUserSeq {
+
+    //redis
+    //uid friend 10
+    //    group 12
+    //    conversation 123
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    public void writeUserSeq(Integer appId,String userId,String type,Long seq){
+        String key = appId + ":" + Constants.RedisConstants.SeqPrefix + ":" + userId;
+        redisTemplate.opsForHash().put(key,type,seq);
+    }
+
+}
