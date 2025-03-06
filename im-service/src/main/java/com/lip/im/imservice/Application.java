e52c26016404973351ad935dbb35400d273fa308
Lipschultz <2562017477@qq.com>
2025-03-06 19:54:32 +0800

    feat: 启动类

--- im-service/src/main/java/com/lip/im/imservice/Application.java
+++ im-service/src/main/java/com/lip/im/imservice/Application.java
@@ -1,5 +1,9 @@
 package com.lip.im.imservice;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: Elon
  * @title: Application
@@ -7,5 +11,14 @@ package com.lip.im.imservice;
  * @description: TODO
  * @date: 2025/3/6 17:13
  */
+@SpringBootApplication(scanBasePackages = {"com.lip.im.imservice",
+        "com.lip.im.model"})
+@MapperScan("com.lip.im.imservice.*.dao.mapper")
+//导入用户资料，删除用户资料，修改用户资料，查询用户资料
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
